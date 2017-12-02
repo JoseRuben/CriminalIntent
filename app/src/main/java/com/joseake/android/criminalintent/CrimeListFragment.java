65cffdef6752efae39b382f75448dde4f1f84793
José Rubén Porfirio Ake Vázquez <14470420@itcampeche.edu.mx>
2017-12-02 15:26:05 -0600

    CriminalIntent: Responde a clics del usuario. Muestra un Toast.

--- app/src/main/java/com/joseake/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/joseake/android/criminalintent/CrimeListFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -46,7 +47,8 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView.setAdapter(mAdapter);
     }
 
-    private class CrimeHolder extends RecyclerView.ViewHolder {
+    private class CrimeHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener{
         private Crime mCrime;
         private TextView mTitleTextView;
         private TextView mDateTextView;
@@ -54,6 +56,7 @@ public class CrimeListFragment extends Fragment {
 
         public CrimeHolder(View itemView) {
             super(itemView);
+            itemView.setOnClickListener(this);
 
             mTitleTextView = (TextView) itemView.findViewById(R.id.list_item_crime_title_text_view);
             mDateTextView = (TextView) itemView.findViewById(R.id.list_item_crime_date_text_view);
@@ -65,6 +68,13 @@ public class CrimeListFragment extends Fragment {
             mTitleTextView.setText(mCrime.getTitle());
             mDateTextView.setText(mCrime.getDate().toString());
             mSolvedCheckBox.setChecked(mCrime.isSolved());
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getActivity(),
+                    "Se hizo clic sobre " + mCrime.getTitle(),
+                    Toast.LENGTH_SHORT).show();
         }
     }
     private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
